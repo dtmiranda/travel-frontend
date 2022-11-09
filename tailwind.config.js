5806ed8c586b80c19511b6af20262e0df96d9c5e
dtmiranda <hortadito2@gmail.com>
2022-11-09 12:00:47 -0100

    project bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+ module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html",
+  ],
+  plugins: [],
+  theme: {},
+};
