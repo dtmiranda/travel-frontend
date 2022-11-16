48e5a2e1420b343a8e48bd274013c685d385e3f7
dtmiranda <hortadito2@gmail.com>
2022-11-16 10:37:39 -0100

    some modification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import Cover from '../components/Cover'
@@ -26,11 +25,11 @@ export default function Home() {
         <DestinationCard/>
         <DestinationCard/>
       </section>
-      
 
-
-      
-                
+      <div className='flex justify-center items-center'>
+        <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>      
+      </div>
+                      
       <Footer />
       
     </>
