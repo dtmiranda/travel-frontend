2e0b7a6a2efee4d33684f8d2f57183a3ba901b0e
dtmiranda <hortadito2@gmail.com>
2023-02-07 16:58:16 -0100

    style: edit bunner

--- components/Bunner.tsx
+++ components/Bunner.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image"
-import coverMontain from "../assets/images/cover-montain.jpg"
-import coverPool from "../assets/images/cover-poll.jpg"
+
 import bunner from '../assets/images/bunner.jpg'
 
 
 export default function Bunner(){
   return(
     <>
-      <section className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+    <main>
+      
+    </main>
+      <section className="flex flex-col pt-10 space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
         <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
           <Image
             layout="fill"
@@ -16,12 +18,12 @@ export default function Bunner(){
             alt=''
           />
         </div>
-        <div className="flex justify-between items-start mx-auto max-w-screen-xl px-4 md:px-4">
+        <div className="flex justify-center items-center">
           <div >
-            <h1 className="mb-4 text-4xl py-4 font-extrabold tracking-tight leading-none text-gray-50 md:text-5xl lg:text-6xl">Visit and enjoy 
+            <h1 className="mt-48 text-center text-4xl pb-4 font-extrabold tracking-tight leading-none text-gray-50 md:text-5xl lg:text-6xl">Visit and enjoy 
               <span className="text-gray-900"> Beautiful </span> places in North.
             </h1>
-            <p className="text-lg font-normal text-gray-50 lg:text-xl">Thinking of tacking a break from every day's busy life? <br /> Don't worry, we take care of your trip.</p>
+            <p className="text-center text-lg font-normal text-gray-50 lg:text-xl">Don't worry, we take care of your trip.</p>
           </div>
 
          
