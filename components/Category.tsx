1d77ca8fe84e0e6d40b5bc228ac8f0fb69e24fa9
dtmiranda <hortadito2@gmail.com>
2023-02-07 17:00:34 -0100

    style: redesign bategory buttons

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,35 +1,42 @@
-import { RiHotelLine, RiRestaurant2Line} from 'react-icons/ri'
-import { MdNordicWalking, MdFestival } from 'react-icons/md'
+import { MdNordicWalking, MdFestival, MdBed, MdRestaurant } from 'react-icons/md'
+import { BsBank } from 'react-icons/bs'
 import Link from 'next/link'
 
 
 export default function Category(){
   return(
-    <div className=" relative my-4 flex justify-center flex-wrap">
+    <div className="my-4 flex justify-center flex-wrap">
 
-      <Link href='/stays' className="w-36 p-2 m-2 max-w-sm flex justify-center flex-col items-center bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 ">
+      <Link href='/accomodation' className="categoryButton">
         <div className='p-2 bg-red-50 rounded-full'>
-          <RiHotelLine size={25} color='#ff8066'/>
+          <MdBed size={25} color='#ff8066'/>
         </div>
-        <p className="pt-1 pr-2 pl-2 font-medium text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent">Stays</p>
+        <p className="pt-1 pr-2 pl-2 font-medium text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent">Accomodation</p>
       </Link>
       
 
-      <Link href="/restaurants" className="w-36 p-2 m-2 max-w-sm flex justify-center flex-col items-center bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 ">
+      <Link href="/restaurants" className="categoryButton">
         <div className='p-2 bg-green-50 rounded-full'>
-          <RiRestaurant2Line size={25} color='#7dcea0'/>
+          <MdRestaurant size={25} color='#7dcea0'/>
         </div>
-        <p className="pt-2 pr-2 pl-2 font-medium text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent">Restaurant</p>
+        <p className="pt-2 pr-2 pl-2 font-medium text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent">Food & Drink</p>
       </Link>
 
-      <Link href="#" className="w-36 p-2 m-2 max-w-sm flex justify-center flex-col items-center bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 ">
+      <Link href="/history" className="categoryButton">
+        <div className='p-2 bg-pink-50 rounded-full'>
+          <BsBank size={25} color='#f945df'/>
+        </div>
+        <p className="pt-2 pr-2 pl-2 font-medium text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent">Arte & History</p>
+      </Link>
+
+      <Link href="/trails" className="categoryButton">
         <div className='p-2 bg-blue-50 rounded-full'>
           <MdNordicWalking size={25} color='#5dade2'/>
         </div>
         <p className="pt-2 pr-2 pl-2 font-medium text-gray-600 rounded hover:bg-gray-100 md:hover:bg-transparent">Trails</p>
       </Link>
 
-      <Link href="#" className="w-36 p-2 m-2 max-w-sm flex justify-center flex-col items-center bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 ">
+      <Link href="/events" className="categoryButton">
         <div className='p-2 bg-pink-50 rounded-full'>
           <MdFestival size={25} color=' #c39bd3 '/>
         </div>
