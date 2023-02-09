6e387f6d071d501cb028e36eba47f61a28f9063d
dtmiranda <hortadito2@gmail.com>
2023-02-09 10:38:45 -0100

    feat: create about us section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Category from '../components/Category'
@@ -5,6 +6,11 @@ import DestinationCard from '../components/DestinationCard'
 import TrailCard from '../components/TrailCard'
 import Head from 'next/head'
 import Bunner from '../components/Bunner'
+
+import aldeia from '../assets/images/aldeia.jpg'
+import piscina from '../assets/images/piscina.jpg'
+import serra from '../assets/images/serra.jpg'
+
 
 export default function Home() {
   return (
@@ -27,7 +33,7 @@ export default function Home() {
           {/* <Category/> */}
           <Category/>
 
-            <h5 className="mt-16 mb-8 text-2xl font-bold tracking-tight leading-none md:text-3xl lg:text-4xl text-gray-700">
+            <h5 className="text-transparent text-center bg-clip-text bg-gradient-to-r to-blue-700 from-gray-700 mt-16 mb-8 text-2xl font-bold tracking-tight leading-none md:text-3xl lg:text-4xl text-gray-700">
               Popular Destination
             </h5>
 
@@ -45,22 +51,54 @@ export default function Home() {
 
           </section>
 
+
+          <section className=' py-8'>
+            <div className=' flex justify-center flex-col items-center'>
+              <h5 className="text-transparent text-center bg-clip-text bg-gradient-to-r to-blue-700 from-gray-700 mt-16 mb-8 text-2xl font-bold tracking-tight leading-none md:text-3xl lg:text-4xl text-gray-700">
+                About Us
+              </h5>
+              <p className="text-gray-500 text-center sm:text-sm md:text-lg lg:text-xl dark:text-gray-600">
+                We planed beautiful destinations every weekend
+              </p>
+
+            </div>
+
+            <div className='py-4 flex justify-between flex-row items-center'>
+              <div className='max-w-sm rounded-2xl overflow-hidden shadow-lg'>
+                <Image className='aboutImage' src={serra} alt={''}/>
+              </div>
+
+              <div className='m-4 max-w-sm rounded-2xl overflow-hidden shadow-lg'>
+                <Image className='aboutImage' src={aldeia} alt={''}/>
+
+              </div>
+
+              <div className='max-w-sm rounded-2xl overflow-hidden shadow-lg'>
+                <Image className='aboutImage' src={piscina} alt={''}/>
+
+              </div>
+
+
+            </div>
+
+            <div>
+            <p className="text-gray-500 text-center text-xs sm:text-sm md:text-lg lg:text-xl">
+                Arranging a healthy and beautiful vacations is our main gol. We <br/>
+                consider our seccess to be able to offer our users the right <br/>
+                journey and a enjoyable holiday
+              </p>
+
+            </div>
+
+
+
+          </section>
+
+
           
 
-          <div className='flex justify-center'>
-            <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 ">See more</button>
-
-          </div>
-
-
-          <div className=' pt-16 flex justify-center flex-col items-center'>
-            <h5 className="mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl lg:text-4xl text-gray-700">
-              Explore some <mark className="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">awesome</mark> trails
-            </h5>
-            <p className="text-gray-500 text-center sm:text-sm md:text-base lg:text-lg dark:text-gray-400">Explore somewhere interesting and enjoy the vibes.</p>
-
-          </div>
-
+          
+          
 
           <div className='flex justify-center flex-col, items-center flex-wrap'>
             <TrailCard/>
