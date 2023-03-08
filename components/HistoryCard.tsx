3ec4c1ff3b984e21b8ac224379bea20f57ec1d2b
dtmiranda <hortadito2@gmail.com>
2023-03-08 13:13:23 -0100

    feat: create art&history pages

---
+++ components/HistoryCard.tsx
@@ -0,0 +1,45 @@
+import { MdOutlineStarHalf } from 'react-icons/md'
+
+
+export default function HistoryCard(props: {
+    name: string,
+    coordinate: string,
+    thumbnails_hotels: string,
+    star_range: string,
+    city: string,
+    price: string,
+    
+
+}){
+  return(
+    
+    <>
+        <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img
+            src={props.thumbnails_hotels}
+            ///«alt={props.imageAlt}
+            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+        </div>
+        <div className="mt-4 flex justify-between items-start">
+            <div>
+                <h3 className="text-sm text-gray-700">
+                    <a href="">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {props.name}
+                    </a>
+                </h3>
+                <p className="mt-1 text-sm font-light text-gray-500">{props.city}</p>
+                <p className="text-base font-medium text-green-900">{props.price}  <span className="pl-1 text-base font-light text-gray-700">person</span></p>
+
+            </div>
+            <div className="flex justify-center items-center">
+                <MdOutlineStarHalf size={18} color='#14532d'/>
+                <p className="text-sm font-medium text-green-900">{props.star_range}</p>
+            </div>
+            
+        </div>
+    </>  
+
+  )
+}
---
+++ pages/history.tsx
@@ -0,0 +1,104 @@
+
+import Head from "next/head"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import HistoryCard from "../components/HistoryCard"
+
+const history_places = [
+
+
+  {
+    id: 1,
+    name: 'Campo de Concentração',
+    description:"description",
+    coordinate: "5345345345534",
+    star_range: "4.95",
+    thumbnails_hotels: 'https://m2s5m8t8.rocketcdn.me/wp-content/uploads/campo-tarrafal-1024x682.jpg',
+    city: "Tarrafal",
+    price: '$35',
+    
+  }, {
+    id: 2,
+    name: 'Campo de Concentração',
+    description:"description",
+    coordinate: "5345345345534",
+    star_range: "4.95",
+    thumbnails_hotels: 'https://m2s5m8t8.rocketcdn.me/wp-content/uploads/campo-tarrafal-1024x682.jpg',
+    city: "Tarrafal",
+    price: '$35',
+    
+  }, {
+    id: 3,
+    name: 'Campo de Concentração',
+    description:"description",
+    coordinate: "5345345345534",
+    star_range: "4.95",
+    thumbnails_hotels: 'https://m2s5m8t8.rocketcdn.me/wp-content/uploads/campo-tarrafal-1024x682.jpg',
+    city: "Tarrafal",
+    price: '$35',
+    
+  }, {
+    id: 4,
+    name: 'Campo de Concentração',
+    description:"description",
+    coordinate: "5345345345534",
+    star_range: "4.95",
+    thumbnails_hotels: 'https://m2s5m8t8.rocketcdn.me/wp-content/uploads/campo-tarrafal-1024x682.jpg',
+    city: "Tarrafal",
+    price: '$35',
+    
+  },
+  // More products...
+  ]
+
+export default function Accomodations(){
+  return (
+    <>
+       <Head>
+        <title>
+          TravelNorth
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <section className="mt-8">
+      <div className="mx-auto max-w-2xl lg:text-center">
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="text-3xl font-bold tracking-tight text-green-900 sm:text-4xl">Art</span> & History
+          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Come and learn a little about our history and appreciate what our artists do.
+          </p>
+        </div>
+      
+      </section>
+
+      <section className="bg-white">
+        <div className="mx-auto max-w-2xl pb-16 px-4 sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {history_places.map((history_place) => {
+              return(
+                <div key={history_place.id} className="group relative">
+
+                  <HistoryCard {...history_place}/>
+
+                </div>
+              )
+            }  
+            )}
+            
+          </div>
+        </div>
+      </section>
+
+
+      <Footer/>
+    
+    </>
+    
+  )
+
+}
