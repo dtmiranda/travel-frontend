697c2da8a91491c6b4a1464eb2921718865a905f
dtmiranda <hortadito2@gmail.com>
2023-03-09 17:03:07 -0100

    feat: create trail page

--- components/TrailCard.tsx
+++ components/TrailCard.tsx
@@ -1,22 +1,54 @@
-import Image from "next/image";
 
-import cardImage from '../assets/images/cover-poll.jpg'
+import { MdOutlineStarHalf } from 'react-icons/md'
 
-export default function NavBar(){
+
+export default function TrailCard(props: {
+    name: string,
+    description: string,
+    trail_thumbnails: string,
+    start_coordinate: string,
+    end_coordinate: string,
+    price: string,
+    duration: string,
+    star_range: string,
+    city:string
+    
+
+}){
   return(
 
-    <div className="my-5 mx-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow-md">
-        <a href="#">
-            <Image className="rounded-t-lg" src={cardImage} alt="" />
-        </a>
-        <div className="p-5">
-            <a href="#">
-                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-600">Noteworthy technology acquisitions 2021</h5>
-            </a>
+    <>
+        <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img
+            src={props.trail_thumbnails}
+            ///«alt={props.imageAlt}
+            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+        </div>
+        <div className="mt-4 flex justify-between items-start">
+            <div>
+                <h3 className="text-sm text-gray-700">
+                    <a href="">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {props.name}
+                    </a>
+                </h3>
+                <p className="mt-1 text-sm font-light text-gray-500">{props.city}</p>
+                <p className="text-base font-medium text-green-900">{props.price}  <span className="pl-1 text-base font-light text-gray-700">person</span></p>
+
+            </div>
+            <div >
+              <div className="flex justify-center items-center">
+                <MdOutlineStarHalf size={18} color='#14532d'/>
+                <p className="text-sm font-medium text-green-900">{props.star_range}</p>
+              </div>
+              <p className="text-sm  text-end font-light text-green-900">{props.duration} min</p>
+
+            </div>
+            
             
         </div>
-    </div>
-
+    </>  
 
   )
 }
---
+++ pages/trails.tsx
@@ -0,0 +1,138 @@
+
+import Head from "next/head"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import TrailCard from "../components/TrailCard"
+
+const trails = [
+
+
+  {
+    id: 1,
+    name: 'Trilha de Serra Malagueta',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis dignissimos quos sint excepturi blanditiis labore dolore. Eligendi deserunt nulla dignissimos facilis quos. Impedit, excepturi est cum sit iusto quisquam.",
+    trail_thumbnails: 'https://hotelrouxinol.com.br/wp-content/uploads/2019/02/saiba-aqui-quais-sao-as-5-melhores-trilhas-em-santa-catarina-719x480.jpeg',
+    start_coordinate: "5345345345534",
+    end_coordinate: "5345345345534",
+    price: '$35',
+    duration: "45",
+    star_range: "4.95",
+    city: "Calheta",
+    
+  },{
+    id: 2,
+    name: 'Trilha de Serra Malagueta',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis dignissimos quos sint excepturi blanditiis labore dolore. Eligendi deserunt nulla dignissimos facilis quos. Impedit, excepturi est cum sit iusto quisquam.",
+    trail_thumbnails: 'https://hotelrouxinol.com.br/wp-content/uploads/2019/02/saiba-aqui-quais-sao-as-5-melhores-trilhas-em-santa-catarina-719x480.jpeg',
+    start_coordinate: "5345345345534",
+    end_coordinate: "5345345345534",
+    price: '$35',
+    duration: "45",
+    star_range: "4.95",
+    city: "Calheta",
+    
+  },{
+    id: 3,
+    name: 'Trilha de Serra Malagueta',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis dignissimos quos sint excepturi blanditiis labore dolore. Eligendi deserunt nulla dignissimos facilis quos. Impedit, excepturi est cum sit iusto quisquam.",
+    trail_thumbnails: 'https://hotelrouxinol.com.br/wp-content/uploads/2019/02/saiba-aqui-quais-sao-as-5-melhores-trilhas-em-santa-catarina-719x480.jpeg',
+    start_coordinate: "5345345345534",
+    end_coordinate: "5345345345534",
+    price: '$35',
+    duration: "45",
+    star_range: "4.95",
+    city: "Calheta",
+    
+  },{
+    id: 4,
+    name: 'Trilha de Serra Malagueta',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis dignissimos quos sint excepturi blanditiis labore dolore. Eligendi deserunt nulla dignissimos facilis quos. Impedit, excepturi est cum sit iusto quisquam.",
+    trail_thumbnails: 'https://hotelrouxinol.com.br/wp-content/uploads/2019/02/saiba-aqui-quais-sao-as-5-melhores-trilhas-em-santa-catarina-719x480.jpeg',
+    start_coordinate: "5345345345534",
+    end_coordinate: "5345345345534",
+    price: '$35',
+    duration: "45",
+    star_range: "4.95",
+    city: "Calheta",
+    
+  },{
+    id: 5,
+    name: 'Trilha de Serra Malagueta',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis dignissimos quos sint excepturi blanditiis labore dolore. Eligendi deserunt nulla dignissimos facilis quos. Impedit, excepturi est cum sit iusto quisquam.",
+    trail_thumbnails: 'https://hotelrouxinol.com.br/wp-content/uploads/2019/02/saiba-aqui-quais-sao-as-5-melhores-trilhas-em-santa-catarina-719x480.jpeg',
+    start_coordinate: "5345345345534",
+    end_coordinate: "5345345345534",
+    price: '$35',
+    duration: "45",
+    star_range: "4.95",
+    city: "Calheta",
+    
+  },{
+    id: 6,
+    name: 'Trilha de Serra Malagueta',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis dignissimos quos sint excepturi blanditiis labore dolore. Eligendi deserunt nulla dignissimos facilis quos. Impedit, excepturi est cum sit iusto quisquam.",
+    trail_thumbnails: 'https://hotelrouxinol.com.br/wp-content/uploads/2019/02/saiba-aqui-quais-sao-as-5-melhores-trilhas-em-santa-catarina-719x480.jpeg',
+    start_coordinate: "5345345345534",
+    end_coordinate: "5345345345534",
+    price: '$35',
+    duration: "45",
+    star_range: "4.95",
+    city: "Calheta",
+    
+  },
+  
+  // More products...
+  ]
+
+export default function Trails(){
+  return (
+    <>
+       <Head>
+        <title>
+          TravelNorth
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <section className="mt-8">
+      <div className="mx-auto text-center max-w-2xl lg:text-center">
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Come and <span className="text-3xl font-bold tracking-tight text-green-900 sm:text-4xl">discover</span> our incredible 
+          <span className="text-3xl font-bold tracking-tight text-green-900 sm:text-4xl"> trails</span>
+        </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          There is so many incredible trails in the north that you can explore with us.
+        </p>
+      </div>
+      
+      </section>
+
+      <section>
+        <div className="mx-auto max-w-2xl pb-16 px-4 sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {trails.map((trail) => {
+              return(
+                <div key={trail.id} className="group relative">
+
+                  <TrailCard {...trail}/>
+
+                </div>
+              )
+            }  
+            )}
+            
+          </div>
+        </div>
+      </section>
+
+
+      <Footer/>
+    
+    </>
+    
+  )
+
+}
